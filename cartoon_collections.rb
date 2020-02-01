0b7605828e8b93d7fbe3965cd0b44e4414797187
Chance cline <clinechance@gmail.com>
2020-02-01 02:54:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,6 @@ def roll_call_dwarves(array)
      puts "#{index+1}. #{name}"
    end 
  end 
- roll_call_dwarves(dwarves)
 
 
 def summon_captain_planet(array)
@@ -11,11 +10,6 @@ def summon_captain_planet(array)
      word.capitalize + "!"
    end 
  end 
-
- summon_captain_planet(planeteer_calls)
-
- short_words = ["puff", "go", "two"]
- assorted_words = ["two", "go", "industrious", "bop"]
 
  def long_planeteer_calls(array)
    array.any? do |word|       
